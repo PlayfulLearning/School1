29fcb4257b00a2713c00923d701d819013d91de0
Hyejin Im <hyejinim17@gmail.com>
2021-07-23 16:07:54 -0400

    Make filter functional

--- components/Projects.js
+++ components/Projects.js
@@ -8,6 +8,7 @@ export default function Projects(projects, themes, about){
             <div class="project-list">
                 ${SubmitButton(about)}
                 ${ProjectItems(about, projects)}
+                </div>
             </div>
         </div>
     </section>`;
@@ -29,7 +30,6 @@ export function SubmitButton(about){
 }
 
 export function ProjectItems(about, projects){
-    SubmitButton(about);
     return projects.map(d=>`
         
         <div class="project-box">
@@ -57,21 +57,20 @@ export function ProjectItems(about, projects){
 export function handleProjectFilter(data){
     
     let conds = document.querySelectorAll('.filter input[name="project-filter"]');
-    console.log(conds);
+    // console.log(conds);
     conds.forEach(cond=>cond.addEventListener('change', function(event){
         
         let checked = event.target.value; //Array.from(conds).filter(d=>d.checked).map(d=>d.value);
         console.log(checked);
         if (checked==='all'){
-            document.querySelector('.project-list').innerHTML = ProjectItems(data.projects);
+            document.querySelector('.project-list').innerHTML = ProjectItems(data.about, data.projects);
         }else{
-            console.log(data.projects);
             let filtered = data.projects.filter(d=>{
-                return d.id.some(id=>checked === id.toLowerCase());
+                // return d.id.some(id=>checked === checked.toLowerCase());
+                return d.id === checked;
             });
             console.log('filtered', filtered);
-        
-            document.querySelector('.project-list').innerHTML = ProjectItems(filtered);
+            document.querySelector('.project-list').innerHTML += SubmitButton(data.about) + ProjectItems(data.about, filtered);
         }
     
     }));
