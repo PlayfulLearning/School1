aa137349fdff1763010b767e6edba104c74ed860
Hyejin Im <hyejinim17@gmail.com>
2021-07-23 15:14:11 -0400

    Change name to id

--- components/Projects.js
+++ components/Projects.js
@@ -6,24 +6,32 @@ export default function Projects(projects, themes, about){
     <section id="projects">
         <div class="wrapper">
             <div class="project-list">
-                <div class="project-box">
-                    <img src="assets/global/add-placeholder.png" div class="teaser">
-                    <div class="info">
-                        <div class="project-overview">
-                            <div class="project-title">
-                                <a href="${about[0].form}" target="_blank"><strong>Submit Project → </strong></a>
-                            </div>
-                        </div>
-                    </div>
-                </div> 
-                ${ProjectItems(projects)}
+                ${SubmitButton(about)}
+                ${ProjectItems(about, projects)}
             </div>
         </div>
     </section>`;
 }
 
-export function ProjectItems(projects){
+export function SubmitButton(about){
+    return `
+        <div class="project-box">
+            <img src="assets/global/add-placeholder.png" div class="teaser">
+            <div class="info">
+                <div class="project-overview">
+                    <div class="project-title">
+                        <a href="${about[0].form}" target="_blank"><strong>Submit Project → </strong></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+}
+
+export function ProjectItems(about, projects){
+    SubmitButton(about);
     return projects.map(d=>`
+        
         <div class="project-box">
             <img src="${(GetTeaserURL(d.images))}" div class="teaser">
             <div class="info">
@@ -47,21 +55,23 @@ export function ProjectItems(projects){
 
 
 export function handleProjectFilter(data){
+    
     let conds = document.querySelectorAll('.filter input[name="project-filter"]');
     console.log(conds);
     conds.forEach(cond=>cond.addEventListener('change', function(event){
         
         let checked = event.target.value; //Array.from(conds).filter(d=>d.checked).map(d=>d.value);
+        console.log(checked);
         if (checked==='all'){
-            console.log('all clicked');
             document.querySelector('.project-list').innerHTML = ProjectItems(data.projects);
         }else{
-            let filtered = data.themes.filter(d=>{
-                return d.tags.some(tag=>checked === tag.toLowerCase());
+            console.log(data.projects);
+            let filtered = data.projects.filter(d=>{
+                return d.id.some(id=>checked === id.toLowerCase());
             });
             console.log('filtered', filtered);
         
-            document.querySelector('.project-list').innerHTML = ThemeItems(filtered);
+            document.querySelector('.project-list').innerHTML = ProjectItems(filtered);
         }
     
     }));
